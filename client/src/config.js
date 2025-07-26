3fd629f2d71f661fe4505e4ef545fd6f4b2aef61
Sneha0907 <sneha09072001@gmail.com>
2025-07-26 18:03:31 +0530

    updates

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,6 @@
-const API_BASE_URL = "https://talk2text-backend.onrender.com";
+const API_BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://talk2text-backend.onrender.com"
+    : "http://localhost:5000";
+
 export default API_BASE_URL;
