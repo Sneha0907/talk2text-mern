4b3a7137cc574d6b94494c56b983c3b282ee380c
Sneha0907 <sneha09072001@gmail.com>
2025-07-26 16:12:26 +0530

    Fix Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Keep it simple for Netlify
+  base: './', // ✅ Important for Netlify & GitHub Pages
 })
